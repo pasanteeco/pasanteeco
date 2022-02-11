93482439e8d727251622dc223409d7da3891492c
pasanteeco <pasanteeco@gmail.com>
2022-02-11 19:09:22 -0300

    primer commit

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.FormSelect.init(elems, {
         classes : "select_"
     });
+
+    var select_tipo_pedido = document.querySelector("#contenedor_fecha");
+    var select_equipos = document.querySelector("#select_equipos");
+
+    var fecha_pedido = document.querySelector("#fecha_1");
+    var hoy = (new Date()).toISOString().slice(0, 10);
+
+
+    fecha_pedido.value = hoy;
+    fecha_pedido.min = hoy;
   });
+
